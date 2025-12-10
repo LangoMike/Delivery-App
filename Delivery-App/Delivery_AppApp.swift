8a67ed566f1f5b368f83c39fd8620832350ad15d
Mike Lango <langomike@yahoo.com>
2025-12-10 16:42:45 -0500

    Initial Commit

---
+++ Delivery-App/Delivery_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Delivery_AppApp.swift
+//  Delivery-App
+//
+//  Created by Mike Gweth Lango on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Delivery_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
